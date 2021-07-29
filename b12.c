4037278d8649be1ff4d73ad06466f3b8a07772a7
Chineme1 <cduba@ucdavis.edu>
2021-07-29 08:49:29 -0700

    Firstb

---
+++ b12.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("You got this.");
+
+
+}
---
+++ buffer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T, int n>
+class Buffer
+{
+	public:
+		Buffer(void) : size(n) {}
+
+	private:
+		T v[n];
+		int size;
+
+
+};
+
+int main()
+{
+	Buffer<char, 64> cbuf;
+	Buffer<float, 32> fbuf;
+	Buffer<string, 8> sbuf;
+}
