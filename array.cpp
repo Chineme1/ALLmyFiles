6d741d966d2134b3f56473b82dc119533434b69d
Chineme1 <cduba@ucdavis.edu>
2021-07-29 08:48:02 -0700

    Firsta

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+	array<char, 64> c;
+	array<float, 32> f;
+	array<string, 8> s;
+
+	c[24] = 'x';
+	f[12] = 1.23f;
+	s[7] = "abcd";
+}
